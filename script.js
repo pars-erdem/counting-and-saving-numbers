9a622d47ec9fab80ccc004f15f3f27bbe91fdca4
pars-erdem <83398363+pars-erdem@users.noreply.github.com>
2023-07-13 12:08:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ let countStr =  count + " - "
 yazEl.innerText += countStr
 console.log(count)
 }
+//
+prompt("A:")
+        let a=prompt("3+5")
+        console.log((a))
+        let save=confirm("number:")
+        if(save==true){
+            alert(true)
+            console.log(true)
+        }
+        else{
+            alert(false)
+            console.log(false)
+        }
+   switch(a==1){
+            case 1:
+                console.log("saved")
+            break;
+            default:
+                console.log("error")
+            break;
+        }
+        switch(prompt("A:")){
+            case "1":
+                document.write("1")
+            break;
+            default:
+                document.write("not")
+            break;
+        }
+
