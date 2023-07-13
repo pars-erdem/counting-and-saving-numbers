f7c32e394a5be40b860d759178e1b5e14d338d84
pars-erdem <83398363+pars-erdem@users.noreply.github.com>
2023-07-13 12:02:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let countEl = document.getElementById("count-el")
+let kaydetEl = document.getElementById("kaydet-el")
+let yazEl = document.getElementById("yaz-el")
+let count = 0
+
+function erdem(){
+ count += 1
+ countEl.textContent = count
+}
+
+function sıfır(){
+ count =  0
+ countEl.textContent = count
+}
+function kayıt(){
+let countStr =  count + " - "
+yazEl.innerText += countStr
+console.log(count)
+}
