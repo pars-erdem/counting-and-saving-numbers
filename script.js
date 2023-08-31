64f1a8f4a856a4dba8497a6a7332f7a7cd7df3b6
erdem <83398363+pars-erdem@users.noreply.github.com>
2023-08-31 07:43:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -36,38 +36,7 @@ console.log(count)
 
 
 
-prompt("A:")
-        let a=prompt("3+5")
-        console.log((a))
-        let save=confirm("number:")
-        if(save==true){
-            alert(true)
-            console.log(true)
-        }
-        else{
-            alert(false)
-            console.log(false)
-        }
- switch(a==1){
-            case 1:
-                console.log("saved")
-            break;
-            default:
-                console.log("error")
-            break;
- }
-let s1=prompt("B:)
-   switch(prompt("A:")){
-            case "1":
-                document.write("1")
-            break;
-            default:
-                document.write("not")
-            break;
-        }
-if(a>prompt("A:")){
-  alert("ERROR!)
-        }
+
 else{
  a=s1
 }
